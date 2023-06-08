46c14d87219bdc14592a9ec2f36a6d67f1f42cc9
Abraham <nigussieabreham80@gmail.com>
2023-06-08 12:59:37 +0300

    Fixing .env

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Requirement = require("./model/requirements");
 
 const app = express();
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uwydggw.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.CYCLIC_DB_USER}:${process.env.CYCLIC_DB_PASS}@cluster0.uwydggw.mongodb.net/?retryWrites=true&w=majority`;
 
 mongoose
   .connect(uri)
