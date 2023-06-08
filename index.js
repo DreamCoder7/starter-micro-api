6fd742211107651f81d07599d5e3a2d3b4b549be
Abraham <nigussieabreham80@gmail.com>
2023-06-08 12:13:23 +0300

    Adding express and .gitignore

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const express = require("express");
+
+const app = express();
+
+// var http = require("http");
+// http
+//   .createServer(function (req, res) {
+//     console.log(`Just got a request at ${req.url}!`);
+//     res.write("Yo!");
+//     res.end();
+//   })
+//   .listen(process.env.PORT || 3000);
+
+app.get("/", (req, res) => {
+  console.log(`Just got a request at ${req.url}!`);
+  res.send("Yo!");
+  res.end();
+});
+
+app.listen(process.env.PORT || 3000);
